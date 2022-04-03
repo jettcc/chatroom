d96bed32499c68382f6f7762691cc0bbfdbd0fdd
AubreyChen <934913797@qq.com>
2022-04-03 16:11:12 +0800

    feat: 搭建oauth2模块

--- im_oauth2server/src/main/java/com/chat/im/im_oauth2server/ImOauth2serverApplication.java
+++ im_oauth2server/src/main/java/com/chat/im/im_oauth2server/ImOauth2serverApplication.java
@@ -1,9 +1,18 @@
 package com.chat.im.im_oauth2server;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+//@EnableOpenApi
+@MapperScan(basePackages = "com.achobeta.dgut.ab_common.mapper")
 @SpringBootApplication
+@EnableDiscoveryClient
+@ComponentScan(basePackages = {"com.chat.im.im_common", "com.chat.im.im_oauth2server"})
+@EnableScheduling
 public class ImOauth2serverApplication {
 
     public static void main(String[] args) {
