28ec214266aaafd44adcd96127f4f35e5ea3db3d
chovychan <934913797@qq.com>
2022-04-10 23:59:44 +0800

    feat: 配置swagger
    Signed-off-by: chovychan <934913797@qq.com>

---
+++ im_gateway/src/main/java/com/chat/im/im_gateway/config/swagger/GatewaySwaggerProvider.java
@@ -0,0 +1,60 @@
+package com.chat.im.im_gateway.config.swagger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+import java.util.*;
+
+/**
+ * @author: AubreyChen in 2022/3/9
+ */
+@Component
+@Primary
+public class GatewaySwaggerProvider implements SwaggerResourcesProvider {
+
+    /**
+     * swagger2默认的url后缀, gateway目前对OAS_30支持尚不完善，此处仍使用swagger2.0的文件类型
+     */
+    private static final String SWAGGER2_URL = "/v2/api-docs";
+
+    @Autowired
+    private RouteLocator routeLocator;
+
+    /**
+     * 网关应用名称
+     */
+    @Value("${spring.application.name}")
+    private String self;
+
+    @Override
+    public List<SwaggerResource> get() {
+        List<SwaggerResource> resources = new ArrayList<>();
+        List<String> routeHosts = new ArrayList<>();
+        routeLocator.getRoutes()
+                .filter(route -> route.getUri().getHost() != null)
+                .filter(route -> route.getUri().getHost() != null &&
+                        Objects.equals(route.getUri().getScheme(), "lb") &&
+                        !self.equalsIgnoreCase(route.getUri().getHost()))
+                .subscribe(route -> routeHosts.add(route.getUri().getHost()));
+
+        // 记录已经添加过的server，存在同一个应用注册了多个服务在注册中心上
+        Set<String> dealed = new HashSet<>();
+        routeHosts.forEach(instance -> {
+            // 拼接url ，请求swagger的url
+            String url = "/" + instance.toLowerCase() + SWAGGER2_URL;
+            if (!dealed.contains(url)) {
+                dealed.add(url);
+                SwaggerResource swaggerResource = new SwaggerResource();
+                swaggerResource.setUrl(url);
+                swaggerResource.setName(instance);
+                resources.add(swaggerResource);
+            }
+        });
+        return resources;
+    }
+}
