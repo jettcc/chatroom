3126815054a1d17e18c3ba8bc317405b9b7a3fd9
chovychan <934913797@qq.com>
2022-04-11 00:04:37 +0800

    feat: 权限认证前缀枚举
    Signed-off-by: chovychan <934913797@qq.com>

---
+++ im_gateway/src/main/java/com/chat/im/im_gateway/utils/AuthConstant.java
@@ -0,0 +1,6 @@
+package com.chat.im.im_gateway.utils;
+
+public class AuthConstant {
+    public static final String AUTHORITY_PREFIX = "ROLE_";
+    public static final String AUTHORITY_CLAIM_NAME = "authorities";
+}
