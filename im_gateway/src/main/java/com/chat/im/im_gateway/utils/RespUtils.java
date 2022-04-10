ba84ac416244c842d9ffb7df2cb95b0d81d756f3
chovychan <934913797@qq.com>
2022-04-11 00:03:34 +0800

    feat:  response策略生成器
    Signed-off-by: chovychan <934913797@qq.com>

---
+++ im_gateway/src/main/java/com/chat/im/im_gateway/utils/RespUtils.java
@@ -0,0 +1,43 @@
+package com.chat.im.im_gateway.utils;
+
+import cn.hutool.json.JSONUtil;
+import com.achobeta.dgut.ab_common.entity.enumeration.GlobalServiceMsgCode;
+import com.achobeta.dgut.ab_common.utils.SystemMsgJsonResponse;
+import org.apache.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ *  response策略生成
+ * @author: chovychan in 2022/4/11
+ */
+@Component
+public class RespUtils {
+    public static Mono<Void> createAccessDeniedResponse(ServerHttpResponse resp, GlobalServiceMsgCode code) {
+        setRespStatus(resp);
+        return resp.writeWith(Mono.just(resp.bufferFactory()
+                .wrap(setRespMsgJsonStr(code).getBytes(StandardCharsets.UTF_8))));
+    }
+
+    private static String setRespMsgJsonStr(GlobalServiceMsgCode code) {
+        return JSONUtil.toJsonStr(findRespMsg(code));
+    }
+
+    private static SystemMsgJsonResponse findRespMsg(GlobalServiceMsgCode code) {
+        if (code == GlobalServiceMsgCode.SUCCESS) {
+            return SystemMsgJsonResponse.success(code);
+        }
+        return SystemMsgJsonResponse.fail(code);
+    }
+
+
+    private static void setRespStatus(ServerHttpResponse resp) {
+        resp.setStatusCode(HttpStatus.OK);
+        resp.getHeaders().add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+    }
+}
