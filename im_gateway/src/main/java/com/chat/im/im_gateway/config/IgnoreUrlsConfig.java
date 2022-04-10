89378d7155a92d3fb900345e16f9540bea367fe9
chovychan <934913797@qq.com>
2022-04-11 00:02:12 +0800

    feat: 配置白名单资源映射, 映射目录配置与application.yml
    Signed-off-by: chovychan <934913797@qq.com>

---
+++ im_gateway/src/main/java/com/chat/im/im_gateway/config/IgnoreUrlsConfig.java
@@ -0,0 +1,20 @@
+package com.chat.im.im_gateway.config;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * 白名单资源映射
+ * @author: chovychan in 2022/4/11
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Component
+@ConfigurationProperties(prefix = "secure.ignore")
+public class IgnoreUrlsConfig {
+    private List<String> urls;
+}
