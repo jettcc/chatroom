f3e2f9d48fa6b82c55124e3601b9cf62b665184d
chovychan <934913797@qq.com>
2022-04-10 11:46:03 +0800

    feat: init gateway
    Signed-off-by: chovychan <934913797@qq.com>

---
+++ im_gateway/src/main/java/com/chat/im/im_gateway/ImGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.chat.im.im_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImGatewayApplication.class, args);
+    }
+
+}
---
+++ im_gateway/src/test/java/com/chat/im/im_gateway/ImGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat.im.im_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
