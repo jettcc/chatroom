fa66c792311cb66c9741ee3ad1afe134b7a8918a
chovychan <934913797@qq.com>
2022-04-11 00:00:34 +0800

    feat: 配置网关权限管理器
    Signed-off-by: chovychan <934913797@qq.com>

---
+++ im_gateway/src/main/java/com/chat/im/im_gateway/config/AuthorizationManager.java
@@ -0,0 +1,39 @@
+package com.chat.im.im_gateway.config;
+
+import com.chat.im.im_gateway.utils.AuthConstant;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.authorization.ReactiveAuthorizationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.server.authorization.AuthorizationContext;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 网关权限管理器
+ * @author: chovychan in 2022/4/11
+ */
+@Component
+@Log4j2
+public class AuthorizationManager implements ReactiveAuthorizationManager<AuthorizationContext> {
+
+    @Override
+    public Mono<AuthorizationDecision> check(Mono<Authentication> mono, AuthorizationContext authorizationContext) {
+        List<String> authorities = new ArrayList<>();
+        authorities.add("ALL");
+        authorities = authorities.stream().map(i -> i = AuthConstant.AUTHORITY_PREFIX + i).collect(Collectors.toList());
+        //认证通过且角色匹配的用户可访问当前路径
+        return mono.filter(Authentication::isAuthenticated)
+                .flatMapIterable(Authentication::getAuthorities)
+                .map(GrantedAuthority::getAuthority)
+                .any(authorities::contains)
+                .map(AuthorizationDecision::new)
+                .defaultIfEmpty(new AuthorizationDecision(false));
+    }
+
+}
