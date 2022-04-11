6fbd1f5a7b324d2968628dd535782302e98a240d
chovychan <934913797@qq.com>
2022-04-11 11:19:01 +0800

    feat: chat server init
    Signed-off-by: chovychan <934913797@qq.com>

---
+++ im_chatServer/src/main/java/com/chat/im/im_chatserver/ImChatServerApplication.java
@@ -0,0 +1,13 @@
+package com.chat.im.im_chatserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImChatServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImChatServerApplication.class, args);
+    }
+
+}
---
+++ im_chatServer/src/test/java/com/chat/im/im_chatserver/ImChatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat.im.im_chatserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImChatServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
