5c080d8847f778bdd9f264b96d0e5e92487feb69
chovychan <934913797@qq.com>
2022-04-27 17:06:36 +0800

    feat: 客户端心跳响应机制
    Signed-off-by: chovychan <934913797@qq.com>

--- im_chatClient/src/main/java/com/chat/im/im_chatclient/hearbeat/HeartbeatRequestHandler.java
+++ im_chatClient/src/main/java/com/chat/im/im_chatclient/hearbeat/HeartbeatRequestHandler.java
@@ -1,4 +1,4 @@
-package com.chat.im.im_chatserver.heartbeat;
+package com.chat.im.im_chatclient.hearbeat;
 
 import com.chat.im.im_common.entity.entity.Invocation;
 import com.chat.im.im_common.entity.inter.MessageHandler;
@@ -8,7 +8,7 @@ import io.netty.channel.Channel;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
 /**
- * 服务端心跳检测
+ * 客户端心跳检测
  * @author: chovychan in 2022/4/27
  */
 @Component
@@ -16,7 +16,7 @@ import org.springframework.stereotype.Component;
 public class HeartbeatRequestHandler implements MessageHandler<HeartbeatRequest> {
     @Override
     public void execute(Channel channel, HeartbeatRequest message) {
-        log.info("[服务端] 收到连接 {} 的心跳...", channel.id());
+        log.info("[客户端] 收到连接 {} 的心跳...", channel.id());
         // 响应心跳
         HeartbeatResponse response = new HeartbeatResponse();
         channel.writeAndFlush(new Invocation(HeartbeatResponse.TYPE, response));
