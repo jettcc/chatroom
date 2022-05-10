1c6212565d47127891b63436cb91f9e7084480f1
chovychan <934913797@qq.com>
2022-05-10 17:06:46 +0800

    fix: 解决了通过Springboot上下文获取bean为null的问题
    Signed-off-by: chovychan <934913797@qq.com>

--- im_chatServer/src/main/java/com/chat/im/im_chatserver/ImChatServerApplication.java
+++ im_chatServer/src/main/java/com/chat/im/im_chatserver/ImChatServerApplication.java
@@ -1,28 +1,27 @@
 package com.chat.im.im_chatserver;
 
+import com.chat.im.im_chatserver.config.netty.WebSocketServer;
+import com.chat.im.im_chatserver.utils.SpringUtil;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
-/**
- * 第一个问题：客户端和服务端单独通信，怎么实现？
- *
- * 第二个问题：单机中websocket主动向所有客户端推送消息如何实现？在集群中如何实现？
- *
- * 第三个问题：单机如何统计同时在线的客户数量？websocket集群如何统计在线的客户数量呢？
- *
- * 第四个问题：由于客户端和websocket服务器集群中的某个节点建立长连接是随机的，如何解决服务端向某个或某些部分客户端推送消息？
- *
- * 第五个问题：websocket服务端周期性向客户端推送消息，单机或集群中如何实现？
- *
- * 第六个问题：websocket集群中一个客户端向其他客户端主动发送消息，如何实现？
- *
- * @author: chovychan in 2022/4/11
- */
 @SpringBootApplication
+@EnableDiscoveryClient
+@MapperScan("com.chat.im.im_common.mapper")
+@ComponentScan(basePackages = {"com.chat.im.im_common", "com.chat.im.im_chatserver"})
 public class ImChatServerApplication {
+    @Bean
+    public SpringUtil getSpringUtil() {
+        return new SpringUtil();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ImChatServerApplication.class, args);
+        WebSocketServer.run();
     }
 
 }
